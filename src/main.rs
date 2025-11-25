f05469056ac806ccb70ad082946ed8bc9d1fb7ae
Faiq Najib <34987164+fanajib5@users.noreply.github.com>
2025-11-25 11:40:28 +0700

    add is palindrome checker function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn is_palindrome(s: &str) -> bool {
+    let chars: Vec<char> = s.chars().collect();
+    let mut left = 0;
+    let mut right = chars.len() - 1;
+
+    while left < right {
+      if chars[left] != chars[right] {
+        return false;
+      }
+
+      left += 1;
+      right -= 1;
+    }
+
+    true
+}
